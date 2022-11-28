223ba042de02212bdbf99d400670fcebfdf02ee0
7figurelarp <113866580+7figurelarp@users.noreply.github.com>
2022-11-27 19:02:48 -0800

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix =
+`
+Write me a detailed Twitter thread by a professional copywriter about the following topic:
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 1.0,
+    max_tokens: 800,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
